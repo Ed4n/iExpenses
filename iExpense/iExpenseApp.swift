3a4804958f283a3cfcfac1142427c3bef22da531
Edgardo Valencia <edgardo.avalencia@gmail.com>
2024-12-10 11:26:09 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Edgardo Valencia on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
